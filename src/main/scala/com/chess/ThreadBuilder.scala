982499e06d326c690529fe17c240559b8123e829
aandrosov <coffee-04@yandex.ru>
2018-02-26 22:42:36 +0300

    worked worker

--- src/main/scala/com/chess/ThreadBuilder.scala
+++ src/main/scala/com/chess/ThreadBuilder.scala
@@ -1,6 +1,6 @@
 package com.chess
 
-import com.chess.figure.ChessShape
+import com.chess.figure.{ChessShape, FigureOnField}
 
 import scala.collection.mutable
 
@@ -11,53 +11,57 @@ object ThreadBuilder {
 
   def createWorker(horizontal: Int, vertical: Int, setFigures: Array[ChessShape], chessField: (Int, Int)): Int = {
     if(setFigures.isEmpty) throw new RuntimeException("setFiguresIsEmpty")
-    val sortedCollectionFigures = setFigures.sortBy(figure => figure.priority)
+    var sortedCollectionFigures = setFigures.sortBy(figure => figure.priority).map(figure => FigureOnField(figure, 1, 1))
     var count: Int = 0
 //    new Thread() {
 //      override def run(): Unit = {
-        setFigures(0).placeHorizontal = horizontal
-        setFigures(0).placeVertical = vertical
+        sortedCollectionFigures(0).horizontal = horizontal
+        sortedCollectionFigures(0).vertical = vertical
+    println(s"set ${sortedCollectionFigures(0).vertical = vertical}")
 
 
 //        var usedFigures: mutable.MutableList[ChessShape] = mutable.MutableList[ChessShape](setFigures(0))
 
         abcd(sortedCollectionFigures, chessField, 1)
 
-        def abcd(sortedCollectionFigures: Array[ChessShape], chessField: (Int, Int), numberFigures: Int): Unit = {
-          for(hor <- sortedCollectionFigures(numberFigures).placeHorizontal to chessField._1){
-            for(vert <- sortedCollectionFigures(numberFigures).placeVertical to chessField._2) {
-              sortedCollectionFigures(numberFigures).placeHorizontal = hor
-              sortedCollectionFigures(numberFigures).placeVertical = vert
-              if(!isBrokenPlace(sortedCollectionFigures.take(numberFigures), sortedCollectionFigures(numberFigures), hor, vert)) {
+        def abcd(sortedCollectionFigures: Array[FigureOnField], chessField: (Int, Int), numberFigures: Int): Unit = {
+          for(hor <- sortedCollectionFigures(numberFigures).horizontal to chessField._1){
+            for(vert <- sortedCollectionFigures(numberFigures).vertical to chessField._2) {
+              sortedCollectionFigures(numberFigures).horizontal = hor
+              sortedCollectionFigures(numberFigures).vertical = vert
+              if(!isBrokenPlace(sortedCollectionFigures.take(numberFigures), sortedCollectionFigures(numberFigures))) {
                 if(numberFigures + 1 == sortedCollectionFigures.length) {
                   count += 1
                 }else {
-                  sortedCollectionFigures(numberFigures).placeHorizontal = hor
-                  sortedCollectionFigures(numberFigures).placeVertical = vert
-                  abcd(sortedCollectionFigures, chessField, numberFigures + 1)
+                  sortedCollectionFigures(numberFigures).horizontal = hor
+                  sortedCollectionFigures(numberFigures).vertical = vert
+                  val refactorArray = replacePlaces(sortedCollectionFigures, numberFigures)
+
+                  abcd(refactorArray, chessField, numberFigures + 1)
                 }
                 if(hor == chessField._1 && vert == chessField._2) {
                   if(numberFigures == 1) {
                     return
                   }
-//                  usedFigures += sortedCollectionFigures(numberFigures)
+                    sortedCollectionFigures(numberFigures - 1).vertical = sortedCollectionFigures(numberFigures - 1).vertical + 1
                     abcd(sortedCollectionFigures, chessField, numberFigures - 1)
                 }
               }
               if(hor == chessField._1 && vert == chessField._2) {
                 if(numberFigures + 1 == sortedCollectionFigures.length) {
                   if(numberFigures == 1) return
+                  sortedCollectionFigures(numberFigures - 1).vertical = sortedCollectionFigures(numberFigures - 1).vertical + 1
                   abcd(sortedCollectionFigures, chessField, numberFigures - 1)
                 }
-                sortedCollectionFigures(numberFigures).placeHorizontal = hor
-                sortedCollectionFigures(numberFigures).placeVertical = vert
-                abcd(sortedCollectionFigures, chessField, numberFigures + 1)
+//                sortedCollectionFigures(numberFigures).horizontal = hor
+//                sortedCollectionFigures(numberFigures).vertical = vert
+//                abcd(sortedCollectionFigures, chessField, numberFigures + 1)
+                return
               }
-              sortedCollectionFigures(numberFigures).placeHorizontal = 1
-              sortedCollectionFigures(numberFigures).placeVertical = 1
+              sortedCollectionFigures(numberFigures).horizontal = 1
+              sortedCollectionFigures(numberFigures).vertical = 1
             }
           }
-          println(s"COUNT ${count}")
         }
 //      }
 //    }.start()
@@ -72,11 +76,21 @@ object ThreadBuilder {
     * @param currentFigure: ChessShape - The current figure
     * @return Boolean- returns true if at least one figure brokes other figure
     */
-  private def isBrokenPlace(usedFigures: Array[ChessShape], currentFigure: ChessShape, hor: Int, vert: Int): Boolean = {
+  private def isBrokenPlace(usedFigures: Array[FigureOnField], currentFigure: FigureOnField): Boolean = {
     for(i <- usedFigures.indices) {
-      if(usedFigures(i).brokenField(currentFigure.placeHorizontal, currentFigure.placeVertical) ||
-      currentFigure.brokenField(usedFigures(i).placeHorizontal, usedFigures(i).placeVertical)) return true
+      if(usedFigures(i).figure.brokenField(usedFigures(i).horizontal, usedFigures(i).vertical,
+        currentFigure.horizontal, currentFigure.vertical) ||
+      currentFigure.figure.brokenField(currentFigure.horizontal, currentFigure.vertical,
+        usedFigures(i).horizontal, usedFigures(i).vertical)) {
+        return true
+      }
     }
     false
+  }
+
+  private def replacePlaces(sortedCollectionFigures: Array[FigureOnField], numberOfFigure: Int): Array[FigureOnField] = {
+    val tail = sortedCollectionFigures.takeRight(sortedCollectionFigures.length - numberOfFigure - 1).map(f => f.copy(horizontal = 1, vertical = 1))
+    val head = sortedCollectionFigures.take(numberOfFigure + 1)
+    head ++ tail
   }
 }
